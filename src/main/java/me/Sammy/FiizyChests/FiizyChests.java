b3fc7ac317a35ff0176616c56a9f5494db247513
Sammy <sjvenable74@gmail.com>
2021-06-04 22:48:00 +0100

    Initial commit

---
+++ src/main/java/me/Sammy/FiizyChests/FiizyChests.java
@@ -0,0 +1,15 @@
+package me.Sammy.FiizyChests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FiizyChests extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
